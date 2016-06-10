c75188e02c5ce9e98d14324e07551dcd9c5049af
HuJiaming <hujiaming@szpayu.com>
2016-06-10 16:32:11 +0800

    新增发送消息队列与接受消息队列

---
+++ ReceiveMSMQ/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReceiveMSMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //启用单线程，读取消息队列消息
+            new Thread(MSMQListen).Start();
+        }
+
+        private static void MSMQListen()
+        {
+            while (true)
+            {
+
+                Console.WriteLine("我在执行");
+                ReceiveMessage();
+                //取出队列里的消息
+                //MessageBox.Show(MsgQueue.ReceiveMessage());
+            }
+            // ReSharper disable once FunctionNeverReturns
+        }
+
+        /// <summary>
+        /// 接受消息队列并从队列中接受消息
+        /// </summary>
+        /// <returns></returns>
+        public static string ReceiveMessage()
+        {
+            //Console.WriteLine("我在执行");
+
+            //连接到本地队列
+            MessageQueue myQueue = new MessageQueue(".\\private$\\myQueue");
+            myQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+
+            try
+            {
+                var myMessage = myQueue.Receive();
+
+                //myMessage.Formatter = new XmlMessageFormatter(new Type[] { typeof(Book) });
+
+                string book = (string)myMessage.Body;
+
+                Console.WriteLine(book);
+
+                return book;
+
+                //return string.Format("编号：{0},书名：{1},作者：{2},定价：{3}", book.BookId,
+                //    book.BookName,
+                //    book.BookAuthor,
+                //    book.BookPrice);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            return "";
+        }
+
+    }
+}
---
+++ SendMSMQ/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendMSMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                SendMessage();
+            }
+
+        }
+
+
+
+        /// <summary>
+        /// 连接消息队列并发送消息到队列
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+        public static bool SendMessage()
+        {
+            bool flag = false;
+            try
+            {
+                //连接到本地的队列
+                MessageQueue myQueue = new MessageQueue(".\\private$\\myQueue");
+                Message myMessage = new Message();
+
+                myMessage.Body = "11";
+                myMessage.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+
+                myQueue.Send(myMessage);
+
+                flag = true;
+
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return flag;
+        }
+    }
+}
