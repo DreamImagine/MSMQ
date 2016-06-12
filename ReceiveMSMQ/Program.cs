291ddd235d3ff2f23aff50ae99651916f9122680
348282932 <maxlong.live@icloude.com>
2016-06-12 10:31:02 +0800

    更新多队列

--- ReceiveMSMQ/Program.cs
+++ ReceiveMSMQ/Program.cs
@@ -21,8 +21,10 @@ namespace ReceiveMSMQ
             while (true)
             {
 
-                Console.WriteLine("我在执行");
+                Console.WriteLine("我在循环了");
                 ReceiveMessage();
+                ReceiveMessage2();
+                ReceiveMessage3();
                 //取出队列里的消息
                 //MessageBox.Show(MsgQueue.ReceiveMessage());
             }
@@ -35,10 +37,91 @@ namespace ReceiveMSMQ
         /// <returns></returns>
         public static string ReceiveMessage()
         {
-            //Console.WriteLine("我在执行");
+            Console.WriteLine("我在执行myQueue");
 
             //连接到本地队列
             MessageQueue myQueue = new MessageQueue(".\\private$\\myQueue");
+            myQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+
+            try
+            {
+                var myMessage = myQueue.Receive();
+
+                //myMessage.Formatter = new XmlMessageFormatter(new Type[] { typeof(Book) });
+
+                string book = (string)myMessage.Body;
+
+                Console.WriteLine(book);
+
+                return book;
+
+                //return string.Format("编号：{0},书名：{1},作者：{2},定价：{3}", book.BookId,
+                //    book.BookName,
+                //    book.BookAuthor,
+                //    book.BookPrice);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            return "";
+        }
+
+
+        /// <summary>
+        /// 接受消息队列并从队列中接受消息
+        /// </summary>
+        /// <returns></returns>
+        public static string ReceiveMessage2()
+        {
+            Console.WriteLine("我在执行myQueue2");
+
+            //连接到本地队列
+            MessageQueue myQueue = new MessageQueue(".\\private$\\myQueue2");
+            myQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+
+            try
+            {
+                var myMessage = myQueue.Receive();
+
+                //myMessage.Formatter = new XmlMessageFormatter(new Type[] { typeof(Book) });
+
+                string book = (string)myMessage.Body;
+
+                Console.WriteLine(book);
+
+                return book;
+
+                //return string.Format("编号：{0},书名：{1},作者：{2},定价：{3}", book.BookId,
+                //    book.BookName,
+                //    book.BookAuthor,
+                //    book.BookPrice);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            return "";
+        }
+
+
+
+        /// <summary>
+        /// 接受消息队列并从队列中接受消息
+        /// </summary>
+        /// <returns></returns>
+        public static string ReceiveMessage3()
+        {
+            Console.WriteLine("我在执行myQueue3");
+
+            //连接到本地队列
+            MessageQueue myQueue = new MessageQueue(".\\private$\\myQueue3");
             myQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
 
             try
--- SendMSMQ/Program.cs
+++ SendMSMQ/Program.cs
@@ -14,7 +14,9 @@ namespace SendMSMQ
 
             for (int i = 0; i < 10; i++)
             {
-                SendMessage();
+                SendMessage("11_"+i);
+                SendMessage2("22_"+i);
+                SendMessage3("33_"+i);
             }
 
         }
@@ -26,7 +28,7 @@ namespace SendMSMQ
         /// </summary>
         /// <param name="book"></param>
         /// <returns></returns>
-        public static bool SendMessage()
+        public static bool SendMessage(string sendValue = "11")
         {
             bool flag = false;
             try
@@ -35,7 +37,68 @@ namespace SendMSMQ
                 MessageQueue myQueue = new MessageQueue(".\\private$\\myQueue");
                 Message myMessage = new Message();
 
-                myMessage.Body = "11";
+                myMessage.Body = sendValue;
+                myMessage.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+
+                myQueue.Send(myMessage);
+
+                flag = true;
+
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return flag;
+        }
+
+        /// <summary>
+        /// 连接消息队列并发送消息到队列
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+        public static bool SendMessage2(string sendValue = "112")
+        {
+            bool flag = false;
+            try
+            {
+                //连接到本地的队列
+                MessageQueue myQueue = new MessageQueue(".\\private$\\myQueue2");
+                Message myMessage = new Message();
+
+                myMessage.Body = sendValue;
+                myMessage.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+
+                myQueue.Send(myMessage);
+
+                flag = true;
+
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return flag;
+        }
+
+
+        /// <summary>
+        /// 连接消息队列并发送消息到队列
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+        public static bool SendMessage3(string sendValue = "113")
+        {
+            bool flag = false;
+            try
+            {
+                //连接到本地的队列
+                MessageQueue myQueue = new MessageQueue(".\\private$\\myQueue3");
+                Message myMessage = new Message();
+
+                myMessage.Body = sendValue;
                 myMessage.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
 
                 myQueue.Send(myMessage);
