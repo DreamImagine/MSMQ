0ee79a874a155744cc306d8d1e4fccb2bcac6e09
HuJiaming <hujiaming@szpayu.com>
2016-06-13 17:20:09 +0800

    新增 book 类

--- ReceiveMSMQ/Program.cs
+++ ReceiveMSMQ/Program.cs
@@ -22,9 +22,10 @@ namespace ReceiveMSMQ
             {
 
                 Console.WriteLine("我在循环了");
-                ReceiveMessage();
-                ReceiveMessage2();
-                ReceiveMessage3();
+                //ReceiveMessage();
+                //ReceiveMessage2();
+                //ReceiveMessage3();
+                ReceiveMessageBook();
                 //取出队列里的消息
                 //MessageBox.Show(MsgQueue.ReceiveMessage());
             }
@@ -151,5 +152,76 @@ namespace ReceiveMSMQ
             return "";
         }
 
+
+
+        /// <summary>
+        /// 接受消息队列并从队列中接受消息
+        /// </summary>
+        /// <returns></returns>
+        public static string ReceiveMessageBook()
+        {
+            Console.WriteLine("我在执行myQueue5");
+
+            //连接到本地队列
+            MessageQueue myQueue = new MessageQueue(".\\private$\\myBook");
+            myQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(Book) });
+
+            try
+            {
+                var myMessage = myQueue.Receive();
+
+                myMessage.Formatter = new XmlMessageFormatter(new Type[] { typeof(Book) });
+
+                var book = myMessage.Body as Book; ;
+
+                Console.WriteLine(book);            
+
+                Console.WriteLine("编号：{0},书名：{1},作者：{2},定价：{3}", book.BookId,
+                    book.BookName,
+                    book.BookAuthor,
+                    book.BookPrice);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            return "";
+        }
+
+
+    }
+
+    public class Book
+    {
+        private int _BookId;
+        public int BookId
+        {
+            get { return _BookId; }
+            set { _BookId = value; }
+        }
+
+        private string _BookName;
+        public string BookName
+        {
+            get { return _BookName; }
+            set { _BookName = value; }
+        }
+
+        private string _BookAuthor;
+        public string BookAuthor
+        {
+            get { return _BookAuthor; }
+            set { _BookAuthor = value; }
+        }
+
+        private double _BookPrice;
+        public double BookPrice
+        {
+            get { return _BookPrice; }
+            set { _BookPrice = value; }
+        }
     }
 }
--- SendMSMQ/Program.cs
+++ SendMSMQ/Program.cs
@@ -12,13 +12,13 @@ namespace SendMSMQ
         static void Main(string[] args)
         {
 
-            for (int i = 0; i < 10; i++)
-            {
-                SendMessage("11_"+i);
-                SendMessage2("22_"+i);
-                SendMessage3("33_"+i);
-            }
-
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    SendMessage("11_"+i);
+            //    SendMessage2("22_"+i);
+            //    SendMessage3("33_"+i);
+            //}
+            SendMessageBook();
         }
 
 
@@ -112,6 +112,78 @@ namespace SendMSMQ
             }
 
             return flag;
+        }
+
+
+        /// <summary>
+        /// 连接消息队列并发送消息到队列
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+        public static bool SendMessageBook(string sendValue = "113")
+        {
+            bool flag = false;
+            try
+            {
+                //连接到本地的队列
+                MessageQueue myQueue = new MessageQueue(".\\private$\\myBook");
+                Message myMessage = new Message();
+
+                Book book = new Book();
+                book.BookId = 1001;
+                book.BookName = "ASP.NET";
+                book.BookAuthor = "ZhangSan";
+                book.BookPrice = 88.88;
+
+
+
+                myMessage.Body = book;
+                myMessage.Formatter = new XmlMessageFormatter(new Type[] { typeof(Book) });
+
+                myQueue.Send(myMessage);
+
+                flag = true;
+
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return flag;
+        }
+
+    }
+
+
+    public class Book
+    {
+        private int _BookId;
+        public int BookId
+        {
+            get { return _BookId; }
+            set { _BookId = value; }
+        }
+
+        private string _BookName;
+        public string BookName
+        {
+            get { return _BookName; }
+            set { _BookName = value; }
+        }
+
+        private string _BookAuthor;
+        public string BookAuthor
+        {
+            get { return _BookAuthor; }
+            set { _BookAuthor = value; }
+        }
+
+        private double _BookPrice;
+        public double BookPrice
+        {
+            get { return _BookPrice; }
+            set { _BookPrice = value; }
         }
     }
 }
